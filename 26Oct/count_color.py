f44a9ae093da5c501103a76bc86f7e58a4da1717
Iris <yanfguo@vub.ac.be>
2016-10-28 21:29:34 +0200

    small modification

--- 26Oct/count_color.py
+++ 26Oct/count_color.py
@@ -1,14 +1,14 @@
 from pylab import *
 
 
-def count_color(img,color,threshold):
+def compare_img(img,color,threshold):
     newim =[[0 for j in range(len(img[0]))] for i in range(len(img))]
     for i in range(len(img)):
         for j in range(len(img[0])):
-            if color[0]-img[i][j][0]<threshold:
+            if sum(abs(color-img[i][j]))<threshold:
                 newim[i][j]=1
             else:
-                newim[i][j]=1-(color[0]-img[i][j][0]-threshold)
+                newim[i][j]=1-(sum(abs(color-img[i][j]))-threshold)
     return newim
 
 
@@ -17,11 +17,23 @@ red1= imread("color1.png")
 red = red1[0][0]
 blue = imread("color2.png")[0][0]
 figure(1)
-imshow(count_color(mario,red,0.01),cmap='gray')
-title("threshold = 0.1,red")
+imshow(compare_img(mario,red,0.01),cmap='gray')
+title("threshold = 0.01,red")
 figure(2)
-imshow(count_color(mario,red,0.9),cmap='gray')
+imshow(compare_img(mario,red,0.5),cmap='gray')
 title("threshold = 0.5,red")
+show()
+
+mario = imread("mario.png")
+red1= imread("color1.png")
+red = red1[0][0]
+blue = imread("color2.png")[0][0]
+figure(3)
+imshow(compare_img(mario,blue,0.01),cmap='gray')
+title("threshold = 0.01,blue")
+figure(4)
+imshow(compare_img(mario,blue,0.5),cmap='gray')
+title("threshold = 0.5,blue")
 show()
 
 
